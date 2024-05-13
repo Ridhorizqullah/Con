fd61b57723b58465d6cebfe5ce219325ba03a26e
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-13 13:26:15 +0700

    destruktor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class angka {
 private:
-	int* arr;
+	int *arr;
 	int panjang;
 public:
 	angka(int); //construktor
@@ -14,8 +14,8 @@ public:
 
 //definisi member function
 angka::angka(int i) {
-	panjang = i
-		arr = new int[i];
+	panjang = i;
+	arr = new int[i];
 		isiData();
 }
 angka::~angka() {
@@ -25,3 +25,24 @@ angka::~angka() {
 	cout << " alamat arry sudah dilepaskan " <<endl;
 }
 
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+	
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+	cout << endl;
+}
+
+int mai() {
+	angka belajarcpp(3);
+	angka* ptrBelajarcpp = new angka(5);
+	delete ptrBelajarcpp;
+
+	return 0;
+}
