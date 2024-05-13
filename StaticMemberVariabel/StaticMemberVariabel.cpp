25bdda728cad2cb3d93e37af1f46c6cac27b2b09
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-13 10:41:43 +0700

    membuat set id

--- StaticMemberVariabel/StaticMemberVariabel.cpp
+++ StaticMemberVariabel/StaticMemberVariabel.cpp
@@ -16,3 +16,18 @@ public:
 		setID();
 	}
 };
+
+int mahasiswa::nim = 10;
+
+void mahasiswa::setID()
+{
+	id = ++nim;
+}
+
+void mahasiswa::printALL()
+{
+	cout << "ID = " << id << endl;
+	cout << "nama = " << nama << endl;
+	cout << endl;
+}
+
